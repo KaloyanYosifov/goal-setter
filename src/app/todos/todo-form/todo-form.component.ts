b884e19bdffd7a9e28b96fd433020e98c861f78d
Kaloyan Yosifov <kaloqn665@gmail.com>
2019-12-17 20:30:10 +0200

    Improve form using ng Tempalte driven style

--- src/app/todos/todo-form/todo-form.component.ts
+++ src/app/todos/todo-form/todo-form.component.ts
@@ -1,47 +1,43 @@
 /**
  * External dependencies.
  */
-import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+
 /**
  * Internal dependencies.
  */
 import { TodoInterface } from '@app/todos/interfaces/todo.interface';
 import { Todo } from '@app/models/todo.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-todos-todo-form',
     templateUrl: './todo-form.component.html',
     styleUrls: ['./todo-form.component.scss'],
 })
-export class TodoFormComponent implements OnChanges {
+export class TodoFormComponent {
+    @ViewChild('form', { static: false }) protected form: NgForm;
+
     @Output() protected onSubmit: EventEmitter<TodoInterface> = new EventEmitter<TodoInterface>();
     @Output() protected onCancel: EventEmitter<void> = new EventEmitter<void>();
 
     @Input() protected todo: Todo = new Todo(-1, '', '', 1, '');
 
-    protected nameData = '';
-    protected descriptionData = '';
-    protected imageUrlData = '';
-    protected timeToReadData = 0;
-
     constructor() {
     }
 
-    ngOnChanges(changes: SimpleChanges): void {
-        this.nameData = this.todo.getName();
-        this.descriptionData = this.todo.getDescription();
-        this.timeToReadData = this.todo.getLength();
-        this.imageUrlData = this.todo.getImageUrl();
-    }
+    submitForm() {
+        if (this.form.invalid) {
+            return;
+        }
 
-    submitForm(event: Event) {
-        event.preventDefault();
+        const { name, description, imageUrl, timeToRead } = this.form.value;
 
         this.onSubmit.emit({
-            name: this.nameData,
-            description: this.descriptionData,
-            imageUrl: this.imageUrlData,
-            timeToRead: this.timeToReadData,
+            name,
+            description,
+            imageUrl,
+            timeToRead,
         });
     }
 
